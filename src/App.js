aaa29c97243f5c4c8b68c3bf769d5e14c4a2d4c3
Manishka Singh <manishka110518@gmail.com>
2024-08-04 22:45:38 +0530

    updating our page with shop page, collection preview component and collection item componenet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'; 
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+    
+      <Routes>
+        <Route exact path='/' element={<HomePage/>} />
+        <Route path='/shop' element={<ShopPage/>} />
+      </Routes>
+    
   );
 }
 
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
-        <div className='background-image' style={{
-            backgroundImage: `url(${imageUrl})`
-        }} />
-                <div className='content'>
-                    <h1 className='title'>{title.toUpperCase()}</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-)
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+  <div className={`${size} menu-item`}>
+    <Link to={linkUrl} className='menu-item-link'>
+      <div className='background-image' 
+           style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className='content'>
+        <h1 className='title'>{title.toUpperCase()}</h1>
+        <span className='subtitle'>SHOP NOW</span>
+      </div>
+    </Link>
+  </div>
+);
 
 export default MenuItem;
